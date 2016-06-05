92fff1b9f1f94d24e1c7d13337e174b2d2bbbd2a
thiennguyen <thiennguyen@2NPF1W1>
2016-06-05 20:50:50 +0700

    Add project files.

---
+++ IDECAF_QLGV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IDECAF_QLGV.Startup))]
+namespace IDECAF_QLGV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
